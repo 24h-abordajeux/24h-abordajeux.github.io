810d9f04486d88372e0315c9da399181e1755bd7
Martin Künzi <kunzi.martin@gmail.com>
2025-08-25 00:20:55 +0200

    merge this in dev

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue'
+import { computed } from 'vue'
 import Header from './Header.vue'
 import Footer from './Footer.vue'
 import Program from './Program.vue'
 import PracticalInformations from './PracticalInformations.vue'
 import { useMainStore } from '../store/main.store'
 import Home from './Home.vue'
-import events from "/src/assets/events.json"
-import events_rpg from "/src/assets/events_rpg.json"
 import Volunteer from './Volunteer.vue'
 
 const mainStore = useMainStore()
@@ -16,30 +14,143 @@ const page = computed(() => mainStore.page)
 
 const isMobile = window.screen.height > window.screen.width
 
-/**
- * 
- */
 
-// TODO : replace that with script
+// TODO : replace that with script from google api
 function fetchEvents() {
-  const events_plus = events
-/*   events_plus.push({
-    "title": "Tournoi de Magic, the gathering Duel Commander - 64 places",
-    "description": "À 10h30, un tournoi de Magic the Gathering, au format Duel Commander, est organisé. Un prize wall minimum est garanti et l'entrée aux 24h du jeux est incluse dans la finance d'inscription.",
+  return [
+    {
+        "title": "Jeux libres",
+        "description": "Durant tout l'événement, nos bénévoles sont à votre disposition pour vous aider à choisir, et comprendre, les jeux mis à dispositions pour l'occasion par l'association, ainsi que par l'Ornithorynque et le Ludesco.",
+        "subscribed": 0,
+        "max": 0,
+        "link": "https://perdu.com",
+        "link_text": "s'inscrire",
+        "show_link": false,
+        "show_subscribed": false
+    },
+    {
+        "title": "Loup Garou Géant",
+        "description": "Durant tout l'événement, nos bénévoles sont à votre disposition pour vous aider à choisir, et comprendre, les jeux mis à dispositions pour l'occasion par l'association, ainsi que par l'Ornithorynque et le Ludesco.",
+        "subscribed": 0,
+        "max": 0,
+        "link": "https://perdu.com",
+        "link_text": "s'inscrire",
+        "show_link": false,
+        "show_subscribed": false
+    },
+    {
+    "title": "Tournoi de Magic, the Gathering Duel Commander - 64 places",
+    "description": "À 10h30, un tournoi de Magic the Gathering, au format Duel Commander, est organisé. Un prize pool minimum est garanti et l'entrée aux 24h du jeux est incluse dans la finance d'inscription de 30.-.",
     "link": "perdu.com",
-    "prize_wall": "a",
-    "poster": "b",
+    "prize_wall": "perdu.com",
+    "poster": "perdu.com",
     "show_link": true,
-    "link_text":"allez viens",
+    "link_text":"S'inscrire",
     "showsubscribed": false,
     "max": 0,
-    "subscribed": 0,
-  }) */
-  return events_plus
+    "subscribed": 0
+    },
+    {
+        "title": "Tournoi de One Piece - 64 places",
+        "description": "Durant tout l'événement, nos bénévoles sont à votre disposition pour vous aider à choisir, et comprendre, les jeux mis à dispositions pour l'occasion par l'association, ainsi que par l'Ornithorynque et le Ludesco.",
+        "subscribed": 0,
+        "max": 0,
+        "link": "https://perdu.com",
+        "link_text": "s'inscrire",
+        "show_link": false,
+        "show_subscribed": false
+    },
+    {
+        "title": "Tournoi de Mind Bug",
+        "description": "Durant tout l'événement, nos bénévoles sont à votre disposition pour vous aider à choisir, et comprendre, les jeux mis à dispositions pour l'occasion par l'association, ainsi que par l'Ornithorynque et le Ludesco.",
+        "subscribed": 12,
+        "max": 12,
+        "link": "https://perdu.com",
+        "link_text": "s'inscrire",
+        "show_link": false,
+        "show_subscribed": true
+    },
+    {
+        "title": "Tournoi de Jass",
+        "description": "Durant tout l'événement, nos bénévoles sont à votre disposition pour vous aider à choisir, et comprendre, les jeux mis à dispositions pour l'occasion par l'association, ainsi que par l'Ornithorynque et le Ludesco.",
+        "subscribed": 0,
+        "max": 0,
+        "link": "https://perdu.com",
+        "link_text": "s'inscrire",
+        "show_link": true,
+        "show_subscribed": false
+    },
+    {
+        "title": "Tournoi d'échecs",
+        "description": "Durant tout l'événement, nos bénévoles sont à votre disposition pour vous aider à choisir, et comprendre, les jeux mis à dispositions pour l'occasion par l'association, ainsi que par l'Ornithorynque et le Ludesco.",
+        "subscribed": 0,
+        "max": 0,
+        "link": "https://perdu.com",
+        "link_text": "s'inscrire",
+        "show_link": false,
+        "show_subscribed": true
+    },
+    {
+        "title": "Initiation aux échecs",
+        "description": "Durant tout l'événement, nos bénévoles sont à votre disposition pour vous aider à choisir, et comprendre, les jeux mis à dispositions pour l'occasion par l'association, ainsi que par l'Ornithorynque et le Ludesco.",
+        "subscribed": 5,
+        "max": 12,
+        "link": "https://perdu.com",
+        "link_text": "s'inscrire",
+        "show_link": true,
+        "show_subscribed": true
+    },
+    {
+        "title": "Tournoi de Pierre - Feuille - Ciseau",
+        "description": "Durant tout l'événement, nos bénévoles sont à votre disposition pour vous aider à choisir, et comprendre, les jeux mis à dispositions pour l'occasion par l'association, ainsi que par l'Ornithorynque et le Ludesco.",
+        "subscribed": 0,
+        "max": 0,
+        "link": "https://perdu.com",
+        "link_text": "Aller sur la page d'inscription",
+        "show_link": true,
+        "show_subscribed": true
+    },
+    {
+        "title": "Le Grand Quizz de Tard dans la nuit",
+        "description": "Durant tout l'événement, nos bénévoles sont à votre disposition pour vous aider à choisir, et comprendre, les jeux mis à dispositions pour l'occasion par l'association, ainsi que par l'Ornithorynque et le Ludesco.",
+        "subscribed": 0,
+        "max": 0,
+        "link": "https://perdu.com",
+        "link_text": "Aller sur la page d'inscription",
+        "show_link": true,
+        "show_subscribed": true
+    },
+    {
+        "title": "Le Draft de Minuit",
+        "description": "Durant tout l'événement, nos bénévoles sont à votre disposition pour vous aider à choisir, et comprendre, les jeux mis à dispositions pour l'occasion par l'association, ainsi que par l'Ornithorynque et le Ludesco.",
+        "subscribed": 0,
+        "max": 0,
+        "link": "https://perdu.com",
+        "link_text": "Aller sur la page d'inscription",
+        "show_link": true,
+        "show_subscribed": true
+    },
+    {
+        "title": "Du Jeu de Rôle",
+        "description": "Durant tout l'événement, nos bénévoles sont à votre disposition pour vous aider à choisir, et comprendre, les jeux mis à dispositions pour l'occasion par l'association, ainsi que par l'Ornithorynque et le Ludesco.",
+        "subscribed": 0,
+        "max": 0,
+        "link": "https://perdu.com",
+        "link_text": "Aller sur la page d'inscription",
+        "show_link": true,
+        "show_subscribed": true
+    },
+]
 }
 
 function fetchRpgEvents() {
-  return events_rpg
+  return [
+    {
+        "title": "Le programme des jeux de rôles arrive prochainement",
+        "description": "Nos maîtres du jeu sont en train de travailler activement pour vous proposer une animation jeu de rôle durant l'intégralité de l'événement. Chaque fois qu'une partie deviendra disponible, nous l'ajouterons à cette page avec un lien pour vous y inscrire."
+    }
+    
+]
 }
 
 </script>
