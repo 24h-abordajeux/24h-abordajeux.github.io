f668e6f89e3cecf0e1b1d6d5790618c48a37b3f8
Martin Künzi <kunzi.martin@gmail.com>
2025-08-25 18:11:01 +0200

    css shit

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -19,7 +19,7 @@ defineProps({
         <div :class="`flexrow ${index%2 ? 'even' : 'odd'}`">
         <div class="event_description">{{ event.description }}</div>
         </div>
-            <div class="" v-if="event.link">
+            <div class="event_infos" v-if="event.link">
                 <a :href="event.link" target="_blank" v-if="event.show_link"><button>{{ event.link_text }}</button></a>
                 <span class="gras" v-if="event.show_subscribed && event.max - event.subscribed > 0"> {{ event.max- event.subscribed }} places restantes</span> <!-- TODO: events.participants to retrieve from internet-->
                 <span class="gras" v-else-if="event.show_subscribed">Complet</span> <!-- TODO: events.participants to retrieve from internet-->
@@ -30,13 +30,14 @@ defineProps({
 <style lang="scss">
 
 .event_title {
+    margin-top: 2px;
     font-weight: 600;
     font-size: large;
 }
 .event_description {
     width: 100%;
-    max-height: 25vh;
     margin-bottom: 2px;
+    overflow-y: auto;
 }
 .gras {
     font-weight: 600;
