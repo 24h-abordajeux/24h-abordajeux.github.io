86abc846db4dc0f6cf95dd0049193a9d0a542d15
Martin Künzi <kunzi.martin@gmail.com>
2025-09-04 18:28:35 +0200

    additional info for TCGS

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -35,33 +35,48 @@ export const EXTRAINFOS = {
     }, mtg_legacy_format: {
         title: "Informations supplémentaires",
         content: [
-            "Le tournoi est un SUL regular",
+            "Le tournoi est un legacy SUL regular",
             "REL: Competitive et decklist requise",
             "Ronde suisse suivie d'un Top 8",
             "Maximum 64 joueurs",
             "Proxies autorisés",
-            "Prize wall garanti jusqu'à 32 participants, et plus de prix si on dépasse",
             "Frais d'inscriptions de 30 CHF, entrée aux 24h du jeu incluse"
         ]
 
     }, mtg_legacy_prize: {
         title: "Récompenses",
         prizes: {
-            "Garanti": [
+            "32 joueurs ou moins": [
                 "1er: Un truc",
                 "2eme: Un autre truc",
                 "3-4: c'est pas mal ça",
                 "5-8: un peu moins bien",
                 "9-12: c'est le geste qui compte",
             ],
-            "Plus ?": [
+            "48 joueurs": [
                 "Chaque entrée, c'est du budget pour des prix en plus :3"
+            ],
+            "64 joueurs": [
+                "ça se voit que c'est pas encore défini ?"
             ]
         }
     }, mtg_night_format: {
-        title: "Informations supplémentaires"
+        title: "Informations supplémentaires",
+        content: [
+            "Le draft se fera avec les trois éditions suivantes : Innistrad Remastered, Duskmourne et Murders at Karlov's manor",
+            "Les cartes seront en anglais",
+            "3 rondes suisses",
+            "Prix de l'inscription de 20 CHF, entrée aux 24h du jeu non-incluse"
+        ]
     }, mtg_night_prize: {
-
+        title: "Récompenses",
+        prizes: {
+            "À Répartir": [
+                "3 boosters Innistrad Remastered, 3 boosters duskmourne, 2 boosters Murder at Karlov's Manor",
+                "Chaque participant repart avec un de ces booster",
+                "Pick order des boosters selon le classement du draft"
+            ]
+        }
     }
 
 }
